4371d4fbc78dd56ba42e63bc56ec7029ec9e3d66
yutong lin <ytl304@nyu.edu>
2019-05-03 15:08:43 -0400

    Updates

---
+++ precache-manifest.db24f263f9341734b487c98e247765af.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d461a6dabe8363fd31f7cf146c11977a",
+    "url": "/working_react/index.html"
+  },
+  {
+    "revision": "8cfa2c9251ec67fd883e",
+    "url": "/working_react/static/css/main.3ebf4b60.chunk.css"
+  },
+  {
+    "revision": "357a6361d91ef2a3029c",
+    "url": "/working_react/static/js/2.59d17c46.chunk.js"
+  },
+  {
+    "revision": "8cfa2c9251ec67fd883e",
+    "url": "/working_react/static/js/main.6f11b490.chunk.js"
+  },
+  {
+    "revision": "2d276e73322f81b1c372",
+    "url": "/working_react/static/js/runtime~main.2923eece.js"
+  },
+  {
+    "revision": "8407adb8924e1a008c8e0678004a7a87",
+    "url": "/working_react/static/media/Sunrise International Demo.8407adb8.otf"
+  },
+  {
+    "revision": "c15042c34fc864fc2620a0f450d90b08",
+    "url": "/working_react/static/media/hi.c15042c3.png"
+  },
+  {
+    "revision": "4042663280ddef1c34f0a4da1f75c1a2",
+    "url": "/working_react/static/media/page1.40426632.gif"
+  },
+  {
+    "revision": "fd5e6c007245106ec1169d5b6460ec60",
+    "url": "/working_react/static/media/page1.fd5e6c00.png"
+  },
+  {
+    "revision": "41fe28e24953d3d50ddf106ad7a0af6e",
+    "url": "/working_react/static/media/page3.41fe28e2.jpg"
+  },
+  {
+    "revision": "72f970b273fd6b1227f6ad06870df09d",
+    "url": "/working_react/static/media/polaroid_camera_photography_temporary_tattoo_1.72f970b2.png"
+  }
+]);
